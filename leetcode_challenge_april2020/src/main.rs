b854943aa8d04aad2317b48562f778212240b7b2
pauldria <paul@myraff.com>
2020-04-04 07:25:55 -0700

    Challenge for 4/3 - tricky!

--- leetcode_challenge_april2020/src/main.rs
+++ leetcode_challenge_april2020/src/main.rs
@@ -2,6 +2,9 @@
 
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::cmp::min;
+use std::cmp::max;
+use std::i32;
 
 fn main() {
     let v1 = vec![2, 2, 1];
@@ -20,6 +23,56 @@ fn main() {
 
     println!("Is {} a happy number? {}", 19, SolutionHappyNumber::is_happy(19));
     println!("Is {} a happy number? {}", 26, SolutionHappyNumber::is_happy(26));
+
+    let v1 = vec![-2, 1, -3];
+
+    println!("Array: {:?}", v1);
+    println!("Backward rolling sum is {:?}", SolutionMaximumSubarray::backward_rolling_sum(&v1));
+    println!("Max-backward is {:?}", SolutionMaximumSubarray::min_backward(&v1));
+    println!("Max-subarray is {}", SolutionMaximumSubarray::max_sub_array(v1));
+
+    let v1 = vec![-2,1,-3,4,-1,2,1,-5,4];
+
+    println!("Array: {:?}", v1);
+    println!("Backward rolling sum is {:?}", SolutionMaximumSubarray::backward_rolling_sum(&v1));
+    println!("Max-backward is {:?}", SolutionMaximumSubarray::min_backward(&v1));
+    println!("Max-subarray is {}", SolutionMaximumSubarray::max_sub_array(v1));
+
+    let v1 = vec![-2, -1];
+
+    println!("Array: {:?}", v1);
+    println!("Backward rolling sum is {:?}", SolutionMaximumSubarray::backward_rolling_sum(&v1));
+    println!("Max-backward is {:?}", SolutionMaximumSubarray::min_backward(&v1));
+    println!("Max-subarray is {}", SolutionMaximumSubarray::max_sub_array(v1));
+
+    let v1 = vec![-1, -2];
+
+    println!("Array: {:?}", v1);
+    println!("Backward rolling sum is {:?}", SolutionMaximumSubarray::backward_rolling_sum(&v1));
+    println!("Max-backward is {:?}", SolutionMaximumSubarray::min_backward(&v1));
+    println!("Max-subarray is {}", SolutionMaximumSubarray::max_sub_array(v1));
+
+    let v1 = vec![1, 2];
+
+    println!("Array: {:?}", v1);
+    println!("Backward rolling sum is {:?}", SolutionMaximumSubarray::backward_rolling_sum(&v1));
+    println!("Max-backward is {:?}", SolutionMaximumSubarray::min_backward(&v1));
+    println!("Max-subarray is {}", SolutionMaximumSubarray::max_sub_array(v1));
+
+    let v1 = vec![-2, -3, -1];
+
+    println!("Array: {:?}", v1);
+    println!("Backward rolling sum is {:?}", SolutionMaximumSubarray::backward_rolling_sum(&v1));
+    println!("Max-backward is {:?}", SolutionMaximumSubarray::min_backward(&v1));
+    println!("Max-subarray is {}", SolutionMaximumSubarray::max_sub_array(v1));
+
+    let v1 = vec![8, -19, 5, -4, 20];
+
+    println!("Array: {:?}", v1);
+    println!("Backward rolling sum is {:?}", SolutionMaximumSubarray::backward_rolling_sum(&v1));
+    println!("Max-backward is {:?}", SolutionMaximumSubarray::min_backward(&v1));
+    println!("Max-subarray is {}", SolutionMaximumSubarray::max_sub_array(v1));
+
 }
 
 struct SolutionSingleNumber {}
@@ -71,5 +124,78 @@ impl SolutionHappyNumber {
             }
             cur_value = new_value;
         }
+    }
+}
+
+struct SolutionMaximumSubarray {}
+
+impl SolutionMaximumSubarray {
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        if n == 0 {
+            return 0;
+        } else if n == 1 {
+            return nums[0];
+        }
+
+        let min_backward = SolutionMaximumSubarray::min_backward(&nums);
+        let v_sum: i32 = nums.iter().sum();
+
+        // Edge case: if we choose nothing in the beginning, then
+        // we look at max_backward[1] or 0 if we choose to pick nothing
+        let mut min_sofar = min(0, min_backward[1]);
+
+        let mut i = 0;
+        let mut rolling_sum = 0;
+
+        while i < n - 2 {
+            println!("i={}, min_sofar={}", i, min_sofar);
+            rolling_sum += nums[i];
+            let min_candidate = rolling_sum + min_backward[i + 2];
+            if min_candidate < min_sofar {
+                min_sofar = min_candidate;
+            }
+            i += 1;
+        }
+
+        // Edge case: at the end, we can't utilize the max_backward and only can look at
+        // rolling sum. Note the duality - here we do NOT use max_backward.
+        rolling_sum += nums[i];
+        println!("i={}, min_sofar={}", i, min_sofar);
+        if rolling_sum < min_sofar {
+            min_sofar = rolling_sum;
+        }
+
+        return v_sum - min_sofar;
+    }
+
+    // We could also choose to pick nothing!
+    pub fn min_backward(v: &Vec<i32>) -> Vec<i32> {
+        let v_brs = SolutionMaximumSubarray::backward_rolling_sum(v);
+        let n = v.len();
+        let mut newv = vec![0; n];
+        let mut i = n-1;
+        newv[i] = min(0, v[i]);
+        i -= 1;
+        while i > 0 {
+            newv[i] = min(newv[i+1], v_brs[i]);
+            i -= 1;
+        }
+        newv[i] = min(newv[i+1], v_brs[i]);
+        return newv;
+    }
+
+    pub fn backward_rolling_sum(v: &Vec<i32>) -> Vec<i32> {
+        let n      = v.len();
+        let mut newv = vec![0; n];
+        let mut i    = n - 1;
+        newv[i] = v[i];
+        i -= 1;
+        while i > 0 {
+            newv[i] = newv[i + 1] + v[i];
+            i -= 1;
+        }
+        newv[i] = newv[i + 1] + v[i];
+        return newv;
     }
 }
