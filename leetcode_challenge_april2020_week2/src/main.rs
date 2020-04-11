087ee56db1aff1b4ac43ad64396716db45be0329
pauldria <paul@myraff.com>
2020-04-11 10:20:14 -0700

    First non-working solution for https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3293/ - we need to work with references instead.

--- leetcode_challenge_april2020_week2/src/main.rs
+++ leetcode_challenge_april2020_week2/src/main.rs
@@ -1,3 +1,7 @@
+use std::cell::RefCell;
+use std::cmp::max;
+use std::rc::Rc;
+
 fn main() {
     let v: Option<Box<ListNode>> = Some(Box::new(ListNode {
         val: 1,
@@ -21,6 +25,44 @@ fn main() {
     println!("{} {}", a, b);
     let m = SolutionBackspace::backspace_compare(a, b);
     println!("  --> {}", m);
+
+    let mut min_stack = MinStack::new();
+    println!("{:?}", min_stack);
+    min_stack.push(2147483646);
+    min_stack.push(2147483646);
+    min_stack.push(2147483647);
+    let top = min_stack.top();
+    println!("Just got top = {}, stack is {:?}", top, min_stack);
+    min_stack.pop();
+    println!("Just popped, stack is {:?}", min_stack);
+    let cur_min = min_stack.get_min();
+    println!("Just got min = {}, stack is {:?}", cur_min, min_stack);
+    min_stack.pop();
+    println!("Just popped, stack is {:?}", min_stack);
+    let cur_min = min_stack.get_min();
+    println!("Just got min = {}, stack is {:?}", cur_min, min_stack);
+    min_stack.pop();
+    println!("Just popped, stack is {:?}", min_stack);
+    min_stack.push(2147483647);
+    let top = min_stack.top();
+    println!("Just got top = {}, stack is {:?}", top, min_stack);
+    let cur_min = min_stack.get_min();
+    println!("Just got min = {}, stack is {:?}", cur_min, min_stack);
+    min_stack.push(-2147483648);
+    println!("Just pushed -2147483648, stack is {:?}", min_stack);
+    let top = min_stack.top();
+    println!("Just got top = {}, stack is {:?}", top, min_stack);
+    let cur_min = min_stack.get_min();
+    println!("Just got min = {}, stack is {:?}", cur_min, min_stack);
+    min_stack.pop();
+    println!("Just popped, stack is {:?}", min_stack);
+    let cur_min = min_stack.get_min();
+    println!("Just got min = {}, stack is {:?}", cur_min, min_stack);
+
+    let v: i64 = 2147483647;
+    println!("v:i64 = {}", v);
+    println!("(v + v)/2 = {}", (v + v)/2);
+    println!("(v + v)/2 as i32 = {}", ((v + v)/2) as i32);
 }
 
 // Definition for singly-linked list.
@@ -118,5 +160,173 @@ impl SolutionBackspace {
         let t_final: String = t_vec.into_iter().collect();
 
         return s_final == t_final;
+    }
+}
+
+#[derive(Debug)]
+struct MinStack {
+    v: Vec<(i32, i32)>,
+    m: Option<i32>
+}
+
+impl MinStack {
+
+    /** initialize your data structure here. */
+    fn new() -> Self {
+        return
+            MinStack {
+                v: Vec::new(),
+                m: None
+            }
+    }
+
+    fn push(&mut self, x: i32) {
+        match self.m {
+            None            => {
+                self.m = Some(x);
+                self.v.push((x, x));
+            }
+            Some(val) => {
+                // If this is the new min, then we make it so.
+                if x < val {
+                    self.v.push((x, val));
+                    self.m = Some(x);
+                }
+                // If this is not a new min, then we push 2*x - val, which
+                // will always be bigger than val.
+                else {
+                    self.v.push((x, val));
+                }
+            }
+        }
+    }
+
+    fn pop(&mut self) {
+        match self.v.pop() {
+            None            => (),
+            Some((val, m)) => {
+                match self.m {
+                    None                => panic!("[pop] I'm popping a nonempty stack with no min element!"),
+                    Some(min_val) => {
+                        // In this case, we hit the min val and need to update
+                        if m != min_val {
+                            self.m = Some(m)
+                        }
+                        else { }
+                    }
+                }
+            }
+        }
+        // Edge case - if v is now empty . . .
+        if self.v.len() == 0 {
+            self.m = None;
+        }
+    }
+
+    fn top(&self) -> i32 {
+        match self.v.last() {
+            None => panic!("[top] There's nothing left in this stack!"),
+            Some((val, m)) => {
+                return *val
+            }
+        }
+    }
+
+    fn get_min(&self) -> i32 {
+        match self.m {
+            None            => panic!("[get_min] There's no minimum value yet!"),
+            Some(val) => return val
+        }
+    }
+}
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+struct SolutionLargestDiameter { }
+
+impl SolutionLargestDiameter {
+    pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            None => 0,
+            Some(val) => {
+                let val = val.borrow();
+                let max_diam_l = SolutionLargestDiameter::diameter_of_binary_tree(val.clone().left);
+                let max_diam_r = SolutionLargestDiameter::diameter_of_binary_tree(val.clone().right);
+                let max_diam_sub = max(max_diam_l, max_diam_r);
+                let mut max_l_path_l = 0;
+                let mut max_l_path_r = 0;
+                let mut max_r_path_l = 0;
+                let mut max_r_path_r = 0;
+                match val.clone().left {
+                    None => {
+                        match val.clone().right {
+                            None => 0,
+                            Some(val_right) => {
+                                max_l_path_r = SolutionLargestDiameter::longest_left_branch(Some(val_right));
+                                max_r_path_r = SolutionLargestDiameter::longest_right_branch(Some(val_right.clone()));
+                                return max(max_diam_sub, 1 + max(max_l_path_r, max_r_path_r));
+                            }
+                        }
+                    },
+                    Some(val_left) => {
+                        match val.clone().right {
+                            None => {
+                                max_l_path_l = SolutionLargestDiameter::longest_left_branch(Some(val_left));
+                                max_r_path_l = SolutionLargestDiameter::longest_right_branch(Some(val_left.clone()));
+                                return max(max_diam_sub, 1 + max(max_l_path_l, max_r_path_l));
+                            },
+                            Some(val_right) => {
+                                max_l_path_r = SolutionLargestDiameter::longest_left_branch(Some(val_right));
+                                max_r_path_r = SolutionLargestDiameter::longest_right_branch(Some(val_right.clone()));
+                                max_l_path_l = SolutionLargestDiameter::longest_left_branch(Some(val_left));
+                                max_r_path_l = SolutionLargestDiameter::longest_right_branch(Some(val_left.clone()));
+                                return max(max_diam_sub, 2 + max(max_l_path_l, max_r_path_l) + max(max_l_path_r, max_r_path_r));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn longest_left_branch(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            None => 0,
+            Some(val) => {
+                let val = val.borrow();
+                let max_l = SolutionLargestDiameter::longest_left_branch(val.clone().left);
+                let max_r = SolutionLargestDiameter::longest_right_branch(val.clone().left);
+                return max(max_l, max_r);
+            }
+        }
+    }
+
+    fn longest_right_branch(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            None => 0,
+            Some(val) => {
+                let val = val.borrow();
+                let max_l = SolutionLargestDiameter::longest_left_branch(val.clone().right);
+                let max_r = SolutionLargestDiameter::longest_right_branch(val.clone().right);
+                return max(max_l, max_r);
+            }
+        }
     }
 }
