1645882b086723289329cec033e2e2d286dd7a19
pauldria <paul@myraff.com>
2020-04-16 08:44:15 -0700

    First - busted, incorrect - solution for parens problem.
    Need to change solution: we need to track forward and see how many stars we need to have to close (, and do the same tracking backward.

--- leetcode_challenge_april2020_week3/src/main.rs
+++ leetcode_challenge_april2020_week3/src/main.rs
@@ -3,6 +3,12 @@ fn main() {
     println!("{:?}", v);
     println!(" --> {:?}", SolutionProductExceptSelf::product_except_self(v));
 
+    //let s = String::from("(())((())()()(*)(*()(())())())()()((()())((()))(*");
+    let s = String::from("((*)(*))((*");
+    //let s = String::from("(*))");
+    println!("{}", s);
+    println!(" --> {}", SolutionValidParens::check_valid_string(s));
+
 }
 
 struct SolutionProductExceptSelf { }
@@ -33,5 +39,88 @@ impl SolutionProductExceptSelf {
             }
         }
         return result;
+    }
+}
+
+struct SolutionValidParens { }
+
+impl SolutionValidParens {
+    pub fn check_valid_string(s: String) -> bool {
+        let n = s.len();
+        let mut rolling_sum: Vec<i32> = vec![0; n];
+        let mut num_stars: Vec<i32>   = vec![0; n];
+        let mut max_pos_needed = 0;
+        if n == 0 {
+            return true;
+        }
+        else if n == 1 {
+            return (s.as_str() == "*");
+        }
+
+        // Start the vectors
+        match s.chars().next().unwrap() {
+            '(' => {
+                rolling_sum[0] = 1;
+            },
+            ')' => {
+                rolling_sum[0] = -1;
+            },
+            '*' => {
+                num_stars[0] = 1;
+            }
+            _ => ()
+        }
+
+        if rolling_sum[0] < 0 {
+            // We have enough stars to compensate, but we need to track
+            if num_stars[0] >= -1*rolling_sum[0] {
+                let disc = num_stars[0] + rolling_sum[0];
+                max_pos_needed = if disc > max_pos_needed { disc } else { max_pos_needed};
+            }
+            else {
+                return false;
+            }
+        }
+
+        let mut idx: usize = 1;
+
+        for c in s[1..n].chars() {
+            // We must continue to propagate regardless!
+            rolling_sum[idx] = rolling_sum[idx-1];
+            num_stars[idx]   = num_stars[idx-1];
+            match c {
+                '(' => {
+                    rolling_sum[idx] = rolling_sum[idx-1] + 1;
+                },
+                ')' => {
+                    rolling_sum[idx] = rolling_sum[idx-1] - 1;
+                },
+                '*' => {
+                    num_stars[idx]   = num_stars[idx-1]  + 1;
+                }
+                _ => ()
+            }
+            println!("{} {:?} {:?}", idx, rolling_sum, num_stars);
+            if rolling_sum[idx] < 0 {
+                // We have enough stars to compensate, but we need to track
+                if num_stars[idx] >= -1*rolling_sum[idx] {
+                    let disc = num_stars[idx] + rolling_sum[idx];
+                    max_pos_needed = if disc > max_pos_needed
+                                        { disc }
+                                     else
+                                        { max_pos_needed };
+                }
+                else {
+                    return false;
+                }
+            }
+
+            idx += 1;
+        }
+        // Now, we need to make sure that we can reconcile everything.
+        // We need to understand how many * we had to turn into (, and then
+        // see if we have enough * left to turn into )
+        // At this point, we know rolling_sum[n-1] >= 0
+        return num_stars[n-1] - max_pos_needed >= rolling_sum[n-1];
     }
 }
